863d822a695f057bfa793c3a3688d9b8f26b23a1
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-09-09 18:35:36 +0300

    ADD Model

---
+++ api_assignment/lib/model/user_model.dart
@@ -0,0 +1,50 @@
+import 'package:api_assignment/model/address.dart';
+import 'package:api_assignment/model/company.dart';
+
+class UserModel {
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.email,
+    required this.address,
+    required this.phone,
+    required this.website,
+    required this.company,
+  });
+  late final int id;
+  late final String name;
+  late final String username;
+  late final String email;
+  late final Address address;
+  late final String phone;
+  late final String website;
+  late final Company company;
+  
+  UserModel.fromJson(Map<String, dynamic> json){
+    id = json['id'];
+    name = json['name'];
+    username = json['username'];
+    email = json['email'];
+    address = Address.fromJson(json['address']);
+    phone = json['phone'];
+    website = json['website'];
+    company = Company.fromJson(json['company']);
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['id'] = id;
+    data['name'] = name;
+    data['username'] = username;
+    data['email'] = email;
+    data['address'] = address.toJson();
+    data['phone'] = phone;
+    data['website'] = website;
+    data['company'] = company.toJson();
+    return data;
+  }
+}
+
+
+
