952a2b0ab0b372bffee259f521e1225d1565f7a8
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-09-09 18:02:08 +0300

    UPD UI version 1

---
+++ api_assignment/lib/networking/api_constant.dart
@@ -0,0 +1,15 @@
+class ApiConstant {
+  static const String baseUrl = "https://jsonplaceholder.typicode.com";
+
+  static const String posts = "/posts";
+
+  static const String comments = "/comments";
+
+  static const String users = "/users";
+
+  static const String albums = "/albums";
+
+  static const String photos = "/photos";
+
+  static const String todos = "/todos";
+}
