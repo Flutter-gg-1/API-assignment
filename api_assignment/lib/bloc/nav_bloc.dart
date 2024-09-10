6433466a61e7d0939738f1b8c40f5986cdaa98ae
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-09-10 15:59:29 +0300

    UPD Bloc nav

--- api_assignment/lib/bloc/nav_bloc.dart
+++ api_assignment/lib/bloc/nav_bloc.dart
@@ -1,13 +1,22 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../core/all_file.dart';
 
 part 'nav_event.dart';
 part 'nav_state.dart';
 
 class NavBloc extends Bloc<NavEvent, NavState> {
+  List<Widget> pages = [const Page1(), const Page2(), const Page3()];
+  int currentIndex = 0;
   NavBloc() : super(NavInitial()) {
-    on<NavEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<ChangePagesEvent>(_changePages);
+  }
+
+  FutureOr<void> _changePages(ChangePagesEvent event, Emitter<NavState> emit) {
+    currentIndex = event.index;
+    emit(ChangePagesState());
   }
 }
--- api_assignment/lib/core/all_file.dart
+++ api_assignment/lib/core/all_file.dart
@@ -1,1 +1,4 @@
 export '../Screen/home_screen.dart';
+export '../Screen/page_1.dart';
+export '../Screen/page_2.dart';
+export '../Screen/page_3.dart';
