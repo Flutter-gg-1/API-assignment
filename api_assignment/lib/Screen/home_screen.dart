5a753d709494a3870689d07e195a9a92de712ea8
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-09-10 13:55:03 +0300

    UPD UI version 4

--- api_assignment/lib/Screen/home_screen.dart
+++ api_assignment/lib/Screen/home_screen.dart
@@ -1,5 +1,3 @@
-import 'package:api_assignment/model/user_model.dart';
-import 'package:api_assignment/networking/api_mixin.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,26 +5,9 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text("Home Screen"),
-              ElevatedButton(
-                  onPressed: () async {
-                    print("object");
-                    List<UserModel> users = await ApiMixin().displayAllUsers();
-                    for (var e in users) {
-                      print(e.name);
-                    }
-                    // print(ApiMixin().display10OnlyOfPhotos());
-                  },
-                  child: const Text("Login"))
-            ],
-          ),
-        ),
+        child: Text('Home Screen'),
       ),
     );
   }
