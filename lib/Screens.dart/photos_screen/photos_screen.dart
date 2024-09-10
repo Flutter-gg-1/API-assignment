200777afa41ec9ff1c93725e38d252c3e98bbbf0
yara albouq <yaramalbouq@gmail.com>
2024-09-11 01:25:18 +0300

    photos  screen

--- lib/Screens.dart/photos_screen/photos_screen.dart
+++ lib/Screens.dart/photos_screen/photos_screen.dart
@@ -1,5 +1,6 @@
 import 'package:api_assignment/Screens.dart/photos_screen/photos_bloc/photos_bloc.dart';
 import 'package:api_assignment/models/photo_model/photo_model.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -14,9 +15,8 @@ class PhotosScreen extends StatelessWidget {
         final bloc = context.read<PhotosBloc>();
         bloc.add(GetPhotosEvent());
         return Scaffold(
-          
-          body: Center(child: SingleChildScrollView(
-              child: BlocBuilder<PhotosBloc, PhotosState>(
+          body: Center(child:
+              SingleChildScrollView(child: BlocBuilder<PhotosBloc, PhotosState>(
             builder: (context, state) {
               if (state is LoadPhotosState) {
                 return const Center(
@@ -28,14 +28,21 @@ class PhotosScreen extends StatelessWidget {
               if (state is SuccessPhotosState) {
                 List<PhotoModel> list = state.photos;
                 return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
                   child: Column(
                     children: List.generate(10, (int index) {
                       final photo = list[index];
                       return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image.network(photo.url), 
-                    );
+                        padding: const EdgeInsets.all(8.0),
+                        child: CachedNetworkImage(
+                          imageUrl: photo.url,
+                          placeholder: (context, url) =>
+                              const CircularProgressIndicator(),
+                          errorWidget: (context, url, error) =>
+                              const Icon(Icons.error, color: Colors.red),
+                        ),
+                      );
                     }),
                   ),
                 );
