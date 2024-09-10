44b7cde5bdf0cdb3d76754876453e6be923cf4ec
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-09-10 16:03:32 +0300

    ADD BlocProvider

--- api_assignment/lib/Screen/home_screen.dart
+++ api_assignment/lib/Screen/home_screen.dart
@@ -1,30 +1,41 @@
+import 'package:api_assignment/bloc/nav_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.verified_user),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.photo),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.photo_library),
-            label: 'Home',
-          ),
-        ],
-      ),
-      body: const SafeArea(
-        child: Center(child: Text('Home Screen')),
-      ),
+    return BlocProvider(
+      create: (context) => NavBloc(),
+      child: Builder(builder: (context) {
+        return BlocBuilder<NavBloc, NavState>(
+          builder: (context, state) {
+            return Scaffold(
+              bottomNavigationBar: BottomNavigationBar(
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.verified_user),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.photo),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.photo_library),
+                    label: 'Home',
+                  ),
+                ],
+              ),
+              body: const SafeArea(
+                child: Center(child: Text('Home Screen')),
+              ),
+            );
+          },
+        );
+      }),
     );
   }
 }
